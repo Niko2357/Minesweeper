205c4e2f9721177fd04dbc3ca1f2aa88a9b121ad
Niko2357 <niktolka16.1@gmail.com>
2024-05-07 21:31:51 +0200

    new class Window

---
+++ src/Window.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Window extends JFrame implements ActionListener {
+    JButton button;
+
+    Window(){
+        JButton button = new JButton();
+        button.setBounds(200, 100, 100, 50);
+        button.setText("Menu");
+
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
+        this.setSize(1000, 1000);
+        this.setVisible(true);
+        this.add(button);
+    }
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == button){
+            System.out.println("yes");
+        }
+    }
+}

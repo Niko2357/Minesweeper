40433806a2ab4642ff46bdbc96a3e72596a6a9b9
Niko2357 <niktolka16.1@gmail.com>
2024-05-26 21:08:26 +0200

    private class from Visual remade as sole class

---
+++ src/ButtonWorks.java
@@ -0,0 +1,53 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ButtonWorks implements ActionListener {
+    private int row;
+    private int column;
+    private Visual visual;
+
+
+    public ButtonWorks(int row, int column, Visual visual) {
+        this.row = row;
+        this.column = column;
+        this.visual = visual;
+    }
+
+    /**
+     * Gets information from the cell. If the cell hides mine, all mines are revealed and game ends.
+     * Otherwise, the game continues.
+     * @param e the event to be processed
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JButton button = (JButton) e.getSource();
+        if(visual.flagMode) {
+            if ("F".equals(button.getText())) {
+                button.setText("");
+            } else {
+                button.setText("F");
+            }
+        }else {
+            if (visual.mines[row][column]) {
+                button.setText("X");
+                visual.revealM();
+                JOptionPane.showMessageDialog(visual, "Game Over!");
+                System.exit(0);
+            } else {
+                visual.revealEmpty(row, column);
+                int count = visual.count(row, column);
+                if (count > 0) {
+                    button.setText(String.valueOf(count));
+                } else {
+                    button.setText("");
+                }
+            }
+
+            button.setEnabled(false);
+        }
+    }
+
+
+
+}

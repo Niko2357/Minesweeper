1f5d173ff3c17a9c9e4047c6a27875ef8a2e2125
Niko2357 <niktolka16.1@gmail.com>
2024-05-27 20:20:43 +0200

    trying out

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,8 @@
+
 public class Main {
     public static void main(String[] args) {
         Intro intro = new Intro();
         intro.menu();
 
-        Game game = new Game();
-        game.playGame();
-
-        Window win = new Window();
     }
 }

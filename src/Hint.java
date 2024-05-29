de964b0a3ff7bd2c5a5e62a16d49b303469405b5
Niko2357 <niktolka16.1@gmail.com>
2024-05-29 22:35:02 +0200

    method for hints

--- src/Hint.java
+++ src/Hint.java
@@ -1,2 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Hint {
+    private Visual visual;
+
+    public Hint(Visual visual){
+        this.visual = visual;
+        giveAHint();
+    }
+
+    public void giveAHint(){
+        boolean[][] mines = visual.mines;
+        JButton[][] buttons = visual.buttons;
+        for(int i = 0; i < mines.length; i++){
+            for(int j = 0; j < mines[i].length; j++){
+                if(!mines[i][j] && buttons[i][j].getText().equals("")){
+                    buttons[i][j].setBackground(Color.CYAN);
+                    return;
+                }
+            }
+        }
+    }
 }

f7ff6b51b72ba8746990ece5ef936d44863acb5e
Niko2357 <niktolka16.1@gmail.com>
2024-05-29 18:52:55 +0200

    added properties and states

--- src/Difficulty.java
+++ src/Difficulty.java
@@ -1,3 +1,23 @@
 public enum Difficulty {
-    EASY, MEDIUM, HARD, GENIUS, IMPOSSIBLE
+    EASY(10, 10),
+    MEDIUM(13, 30),
+    HARD(16, 40),
+    GENIUS(21, 99),
+    IMPOSSIBLE(10, 100);
+
+    protected final int size;
+    protected final int mines;
+
+    Difficulty(int size, int mines){
+        this.size = size;
+        this.mines = mines;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getMines() {
+        return mines;
+    }
 }

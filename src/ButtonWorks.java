384cc2951cacb7d52002e4051dd20e45ec7c84b1
Niko2357 <niktolka16.1@gmail.com>
2024-05-30 13:38:55 +0200

    another if for hints to not be so easy to get(I know its long)

--- src/ButtonWorks.java
+++ src/ButtonWorks.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -13,6 +14,7 @@ public class ButtonWorks implements ActionListener {
         this.column = column;
         this.visual = visual;
     }
+    int clickCount = 10;
 
     /**
      * Gets information from the cell. If the cell hides mine, all mines are revealed and game ends.
@@ -22,44 +24,95 @@ public class ButtonWorks implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         JButton button = (JButton) e.getSource();
-        if(visual.flagMode) {
-            if ("F".equals(button.getText())) {
-                button.setText("");
-                if(visual.mines[row][column]){
-                    visual.foundMines--;
-                }
-            } else {
-                button.setText("F");
-                if(visual.mines[row][column]){
-                    visual.foundMines++;
-                }
-            }
-        }else {
-            if("F".equals(button.getText())){
-                return;
-            }
-            if (visual.mines[row][column]) {
-                button.setText("X");
-                visual.revealM();
-                visual.lost = true;
-                JOptionPane.showMessageDialog(visual, "Game Over!");
-                new Menu();
-                visual.dispose();
-            } else {
-                visual.revealEmpty(row, column);
-                int count = visual.count(row, column);
-                if (count > 0) {
-                    button.setText(String.valueOf(count));
+        if (button.getBackground().equals(Color.CYAN)) {
+            if (clickCount == 0) {
+                if (visual.flagMode) {
+                    if ("F".equals(button.getText())) {
+                        button.setText("");
+                        if (visual.mines[row][column]) {
+                            visual.foundMines--;
+                        }
+                    } else {
+                        button.setText("F");
+                        if (visual.mines[row][column]) {
+                            visual.foundMines++;
+                        }
+                    }
                 } else {
-                    button.setText("");
+                    if ("F".equals(button.getText())) {
+                        return;
+                    }
+                    if (visual.mines[row][column]) {
+                        button.setText("X");
+                        visual.revealM();
+                        visual.lost = true;
+                        int option = JOptionPane.showOptionDialog(visual, "Game Over!", "You lost", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, new Object[]{"Play again", "Menu"}, null);
+                        if (option == 0) {
+                            visual.dispose();
+                            new SelectDiff();
+                        } else if (option == 1) {
+                            visual.dispose();
+                            new Menu();
+                        }
+                        new Menu();
+                        visual.dispose();
+                    } else {
+                        visual.revealEmpty(row, column);
+                        int count = visual.count(row, column);
+                        if (count > 0) {
+                            button.setText(String.valueOf(count));
+                        } else {
+                            button.setText("");
+                        }
+                    }
+                    button.setEnabled(false);
                 }
+                visual.winCheck();
+            } else {
+                clickCount--;
             }
-
-            button.setEnabled(false);
+        } else {
+            if (visual.flagMode) {
+                if ("F".equals(button.getText())) {
+                    button.setText("");
+                    if (visual.mines[row][column]) {
+                        visual.foundMines--;
+                    }
+                } else {
+                    button.setText("F");
+                    if (visual.mines[row][column]) {
+                        visual.foundMines++;
+                    }
+                }
+            } else {
+                if ("F".equals(button.getText())) {
+                    return;
+                }
+                if (visual.mines[row][column]) {
+                    button.setText("X");
+                    visual.revealM();
+                    visual.lost = true;
+                    int option = JOptionPane.showOptionDialog(visual, "Game Over!", "You lost", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, new Object[]{"Play again", "Menu"}, null);
+                    if (option == 0) {
+                        visual.dispose();
+                        new SelectDiff();
+                    } else if (option == 1) {
+                        visual.dispose();
+                        new Menu();
+                    }
+                } else {
+                    visual.revealEmpty(row, column);
+                    int count = visual.count(row, column);
+                    if (count > 0) {
+                        button.setText(String.valueOf(count));
+                    } else {
+                        button.setText("");
+                    }
+                }
+                button.setEnabled(false);
+            }
+            visual.winCheck();
         }
-        visual.winCheck();
     }
-
-
-
 }
+

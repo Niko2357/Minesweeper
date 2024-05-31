43d038b43a90280917cf8d67f149b835309a7d7e
Niko2357 <niktolka16.1@gmail.com>
2024-05-31 14:54:20 +0200

    added new property class SelectFlag and event

--- src/ImageClickListener.java
+++ src/ImageClickListener.java
@@ -4,13 +4,16 @@ import java.awt.event.MouseEvent;
 
 public class ImageClickListener extends MouseAdapter {
     protected String label;
+    protected SelectFlag flag;
 
-    public ImageClickListener(String label){
+    public ImageClickListener(String label, SelectFlag flag){
         this.label = label;
+        this.flag = flag;
     }
 
     @Override
     public void mouseClicked(MouseEvent e){
-        JOptionPane.showMessageDialog(null, label, "flower", JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null,"You chose " +  label, "Chosen flag", JOptionPane.INFORMATION_MESSAGE);
+        flag.dispose();
     }
 }

b9a20f54c27083c818695403df065cb17e6c75ef
Niko2357 <niktolka16.1@gmail.com>
2024-05-31 21:41:31 +0200

    .

--- src/ButtonWorks.java
+++ src/ButtonWorks.java
@@ -15,6 +15,13 @@ public class ButtonWorks implements ActionListener {
         this.visual = visual;
     }
     int clickCount = 10;
+    ImageIcon dirt = new ImageIcon("Floor/dirt.jpg");
+    ImageIcon TNT = new ImageIcon("Floor/TNT.png");
+    String flagAddress = ImageClickListener.flagsChosen.get(ImageClickListener.flagsChosen.size() - 1);
+    ImageIcon flag = new ImageIcon(flagAddress);;
+    ImageIcon obsidian = new ImageIcon("Floor/Obsidian.jpg");
+    ImageIcon grass = new ImageIcon("Floor/grass.png");
+
 
     /**
      * Gets information from the cell. If the cell hides mine, all mines are revealed and game ends.
@@ -22,28 +29,34 @@ public class ButtonWorks implements ActionListener {
      * @param e the event to be processed
      */
     @Override
-    public void actionPerformed(ActionEvent e) {
+    public void actionPerformed(ActionEvent e){
         JButton button = (JButton) e.getSource();
-        if (button.getBackground().equals(Color.CYAN)) {
+        if (button.getIcon().equals(obsidian)) {
             if (clickCount == 0) {
                 if (visual.flagMode) {
-                    if ("F".equals(button.getText())) {
+                    if (flag.equals(button.getIcon())) {
+                        button.setIcon(null);
+                        button.setIcon(grass);
                         button.setText("");
                         if (visual.mines[row][column]) {
                             visual.foundMines--;
                         }
                     } else {
-                        button.setText("F");
+                        button.setIcon(null);
+                        button.setIcon(flag);
+                        button.setText("");
                         if (visual.mines[row][column]) {
                             visual.foundMines++;
                         }
                     }
                 } else {
-                    if ("F".equals(button.getText())) {
+                    if (flag.equals(button.getIcon())) {
                         return;
                     }
                     if (visual.mines[row][column]) {
-                        button.setText("X");
+                        button.setIcon(null);
+                        button.setIcon(TNT);
+                        button.setText("");
                         visual.revealM();
                         visual.lost = true;
                         int option = JOptionPane.showOptionDialog(visual, "Game Over!", "You lost", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, new Object[]{"Play again", "Menu"}, null);
@@ -57,10 +70,11 @@ public class ButtonWorks implements ActionListener {
                     } else {
                         visual.revealEmpty(row, column);
                         int count = visual.count(row, column);
-                        if (count > 0) {
+                        button.setIcon(null);
+                        button.setIcon(dirt);
+                        button.setOpaque(true);
+                        if(count > 0) {
                             button.setText(String.valueOf(count));
-                        } else {
-                            button.setText("");
                         }
                     }
                     button.setEnabled(false);
@@ -71,23 +85,27 @@ public class ButtonWorks implements ActionListener {
             }
         } else {
             if (visual.flagMode) {
-                if ("F".equals(button.getText())) {
+                if (flag.equals(button.getIcon())) {
+                    button.setIcon(null);
+                    button.setIcon(grass);
                     button.setText("");
                     if (visual.mines[row][column]) {
                         visual.foundMines--;
                     }
                 } else {
-                    button.setText("F");
+                    button.setIcon(null);
+                    button.setIcon(flag);
+                    button.setText("");
                     if (visual.mines[row][column]) {
                         visual.foundMines++;
                     }
                 }
             } else {
-                if ("F".equals(button.getText())) {
+                if (flag.equals(button.getIcon())) {
                     return;
-                }
-                if (visual.mines[row][column]) {
-                    button.setText("X");
+                }else if (visual.mines[row][column]) {
+                    button.setIcon(null);
+                    button.setIcon(TNT);
                     visual.revealM();
                     visual.lost = true;
                     int option = JOptionPane.showOptionDialog(visual, "Game Over!", "You lost", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, new Object[]{"Play again", "Menu"}, null);
@@ -99,6 +117,8 @@ public class ButtonWorks implements ActionListener {
                         new Menu();
                     }
                 } else {
+                    button.setIcon(null);
+                    button.setIcon(dirt);
                     visual.revealEmpty(row, column);
                     int count = visual.count(row, column);
                     if (count > 0) {
@@ -108,6 +128,7 @@ public class ButtonWorks implements ActionListener {
                     }
                 }
                 button.setEnabled(false);
+                button.setIcon(null);
             }
             visual.winCheck();
         }
